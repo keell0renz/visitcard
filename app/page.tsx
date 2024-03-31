be10c2ff93fa4ac9f6b07c58ccf31950be157823
Bohdan Agarkov <keell0renz@localhost.localdomain>
2024-04-01 00:18:09 +0200

    Added responsiveness a little bit :)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SocialLink from "@/components/ui/social-link";
 export default function Home() {
   return (
     <main className="p-4 max-w-7xl mx-auto relative w-full pt-20 md:pt-0 flex flex-col justify-center items-center h-[60vh]">
-      <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500 bg-opacity-50 min-h-24">
+      <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-500 bg-opacity-50 min-h-24">
         I love building crazy{" "}
         <span className="bg-clip-text bg-gradient-to-r from-blue-500 via-pink-400 to-red-500">
           shit
@@ -22,21 +22,21 @@ export default function Home() {
         ?
       </p>
       <article className="w-8/12 pt-16 mx-auto flex flex-col">
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex flex-row justify-start items-center space-x-2">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+          <div className="flex flex-col md:flex-row justify-start items-center space-x-2">
             <h1 className="text-xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-400 hover:bg-gradient-to-r hover:from-blue-500 hover:via-pink-400 hover:to-red-500">
               Bohdan Agarkov
             </h1>
             <p className="text-sm text-muted-foreground font-light">
               [ˈboɦdan ˈaɡarkov]
             </p>
-            <a
+            {/* <a
               className="text-sm text-muted-foreground font-light hover:text-blue-500"
               href="mailto:bogdan122305@gmail.com"
               target="_blank"
             >
               bogdan122305@gmail.com
-            </a>
+            </a> */}
             <a
               className="text-sm text-muted-foreground font-light hover:text-blue-500"
               href="https://www.uva.nl/"
