c00218736a418d9178b714998e90de357734958c
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-04-03 22:21:13 +0200

    Deep refactoring of social-link component.

---
+++ components/ui/social-link/icons/index.tsx
@@ -0,0 +1,5 @@
+export { Email as Email } from "./email";
+export { Github as Github } from "./github";
+export { LinkedIn as LinkedIn } from "./linkedin";
+export { Telegram as Telegram } from "./telegram";
+export { Twitter as Twitter } from "./twitter";
