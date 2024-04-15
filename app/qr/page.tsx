c507c4bd6d84dcc8d9a190c83c240a06a56cff0e
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-04-15 12:14:16 +0200

    Added website and nickname to QR code page

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center pt-24">
-      <Image src="/qr.svg" alt="QR Code" width={300} height={300} />
+    <main className="flex flex-col items-center pt-24 space-y-4">
+      <Image
+        src="/qr.svg"
+        alt="QR Code"
+        width={300}
+        height={300}
+        className="mb-3"
+      />
+      <h1 className="text-2xl font-mono">https://keellorenz.com</h1>
+      <h1 className="text-2xl font-mono">@keell0renz</h1>
     </main>
   );
 }
