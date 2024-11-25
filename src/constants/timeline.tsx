049db84deefbf23969213abe8c47d38ab538c30e
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-11-25 19:47:30 +0100

    Updated

--- src/constants/timeline.tsx
+++ src/constants/timeline.tsx
@@ -4,7 +4,7 @@ export const timeline = [
     title: "Software Engineer",
     date: "Aug 2024 - Oct 2024",
     description:
-      "MultiOn is a startup based in Silicon Valley developing autonomous AI agents for the web.",
+      "MultiOn is a startup based in Silicon Valley developing autonomous AI agents for the web",
     responsibilities: [
       "Completed my first one-time paid contract",
       "Worked on internal company projects",
@@ -17,7 +17,7 @@ export const timeline = [
     title: "Software Engineering Intern",
     date: "Mar 2024 - Aug 2024",
     description:
-      "Tiny Fish is a startup based in Silicon Valley developing web extraction and automation AI solutions.",
+      "Tiny Fish is a startup based in Silicon Valley developing web extraction and automation AI solutions",
     responsibilities: [
       "Developed Python SDKs and full-stack web applications",
       "Built autonomous AI agents for web browser automation",
