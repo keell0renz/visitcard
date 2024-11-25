be5b25ea3ad88f7de3492cd93fa6fadefb4be990
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-11-25 18:32:12 +0100

    REDOING WITH NEW TEMPLATE

--- components/ui/social-link/icons/index.tsx
+++
@@ -1,5 +0,0 @@
-export { Email as Email } from "./email";
-export { Github as Github } from "./github";
-export { LinkedIn as LinkedIn } from "./linkedin";
-export { Telegram as Telegram } from "./telegram";
-export { Twitter as Twitter } from "./twitter";
