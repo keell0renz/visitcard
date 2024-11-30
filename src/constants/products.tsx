d2fd04e79ca3a06a75150518ea774b946218a2ee
Bohdan Agarkov <sidi0us.mail@gmail.com>
2024-11-30 18:03:37 +0100

    new projects

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,6 +1,8 @@
 import llmos from "public/images/llm-os.png";
 import langcode from "public/images/langcode.png";
 import multionpydantic from "public/images/multionpydantic.png";
+import shortlink from "public/images/shortlink.png";
+import omniscient from "public/images/omniscient.png";
 
 export const products = [
   {
@@ -35,5 +37,25 @@ export const products = [
     stack: ["Python", "Pydantic", "MultiOn"],
     slug: "multion-pydantic",
     pinned: true,
+  },
+  {
+    href: "https://github.com/keell0renz/shortlink",
+    title: "Shortlink",
+    description:
+      "A simple URL shortener built with Python and FastAPI. My first back-end microservice, created when I was learning principles of back-end development.",
+    thumbnail: shortlink,
+    images: [shortlink],
+    stack: ["Python", "FastAPI", "Docker"],
+    slug: "shortlink",
+  },
+  {
+    href: "https://github.com/keell0renz/omniscient",
+    title: "Omniscient",
+    description:
+      "My first startup which I did in 2023, AI-powered EdTech platform for self-learners, but failed and learned a lot. After that I decided to get an internship at an actual startup and learn from experienced people.",
+    thumbnail: omniscient,
+    images: [omniscient],
+    stack: ["Next.js", "Prisma", "React", "Tailwind", "Clerk"],
+    slug: "omniscient",
   },
 ];
