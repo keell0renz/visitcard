8edba396239bdb2f0918eed829beb5631525d2ca
Bohdan Agarkov <bogdan122305@gmail.com>
2025-04-18 21:49:34 +0200

    Remove unused sitemap and robots files; update base URL in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-// import Footer from "./components/footer";
-import { baseUrl } from './sitemap';
+
+const baseUrl = "https://keellorenz.com"
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
